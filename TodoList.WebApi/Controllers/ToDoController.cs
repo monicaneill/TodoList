3f06d044b0b3ccc9b8688b35e93d8a708fb4ffc4
Monica Neill <Monica.Neill2@jet2.com>
2024-07-24 13:54:53 +0100

    Adding necessary services for Program to be able to host api

---
+++ TodoList.WebApi/Controllers/ToDoController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TodoList.WebApi.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ToDoController : ControllerBase
+{
+    [HttpGet]
+    public IEnumerable<string> Get()
+    {
+        return new string[] { "value1", "value2" };
+    }
+
+    // GET api/ToDoController>/5
+    [HttpGet("{id}")]
+    public string Get(int id)
+    {
+        return $"value {id}";
+    }
+
+    //make make separate one for fetching Completed
+    [HttpGet("{Completed}")]
+    public string Get(int id)
+    {
+        return $"value {id}";
+    }
+
+    // POST api/<ToDoController>
+    [HttpPost]
+    public void Post([FromBody] string value)
+    {
+    }
+
+    // PUT api/<ToDoController>/5
+    [HttpPut("{id}")]
+    public void Put(int id, [FromBody] string value)
+    {
+    }
+
+    // DELETE api/<ToDoController>/5
+    [HttpDelete("{id}")]
+    public void Delete(int id)
+    {
+    }
+}
