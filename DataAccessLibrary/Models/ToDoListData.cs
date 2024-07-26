742333410b8d1084bb2438982f2b430898367170
Monica Neill <Monica.Neill2@jet2.com>
2024-07-26 08:44:39 +0100

    Moving ToDoListDto and updating name to Data Access class to avoid circular dependencies

---
+++ DataAccessLibrary/Models/ToDoListData.cs
@@ -0,0 +1,8 @@
+﻿namespace DataAccessLibrary.Models;
+
+public class ToDoListData
+{
+    public required int Id { get; set; }
+    public required string ItemToDo { get; set; }
+    public required bool Completed { get; set; } = false;
+}
