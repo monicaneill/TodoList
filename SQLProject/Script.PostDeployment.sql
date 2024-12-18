067c0e34e3ab65bee01484418e2f254b269a61ea
Monica Neill <Monica.Neill2@jet2.com>
2024-12-18 15:31:49 +0000

    Adding reseed to Post Deployment Script to re-set identity when delete all items and start again

--- SQLProject/Script.PostDeployment.sql
+++ SQLProject/Script.PostDeployment.sql
@@ -1,8 +1,10 @@
-﻿If not exists (Select 1 from dbo.ToDo)
+﻿If Not Exists (Select 1 from dbo.ToDo)
 Begin
-	Insert into dbo.ToDo (ItemToDo, Completed)
-	Values 
-	('Kiss wife', 1),
-	('Wash dishes', 0),
-	('Teach dog trick', 0);
+    DBCC CHECKIDENT ('dbo.ToDo', RESEED, 0);
+
+    Insert into dbo.ToDo (ItemToDo, Completed)
+    Values 
+    ('Kiss wife', 1),
+    ('Wash dishes', 0),
+    ('Teach dog trick', 0);
 End
